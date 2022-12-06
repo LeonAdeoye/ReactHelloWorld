263745ffcac11f233ea53d9595e9f4c0a3a1d1e1
LeonAdeoye <leon.adeoye@gmail.com>
2022-12-06 23:03:29 +0800

    Added first class component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Greet from '../../reacthelloworld/src/components/Greet'
 import AnotherNameForGreetES6 from '../../reacthelloworld/src/components/GreetES6'
 import { GreetWithoutExport } from '../../reacthelloworld/src/components/GreetWithoutExport'
+import GreetClassComponent from "./components/GreetClassComponent";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Greet/>
         <AnotherNameForGreetES6/>
         <GreetWithoutExport/>
+        <GreetClassComponent/>
       </div>
   );
 }
---
+++ src/components/GreetClassComponent.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class GreetClassComponent extends Component
+{
+    render() {
+        return <h1>Hello Leon</h1>
+    }
+}
+
+export default GreetClassComponent;
