9303310529a53dee5d3aa801d584c36e7cca01a8
LeonAdeoye <leon.adeoye@gmail.com>
2022-12-14 22:43:35 +0800

    fragments

--- src/components/Column.js
+++ src/components/Column.js
@@ -8,8 +8,10 @@ class Column extends Component
         // Without these, you would have to use a div tag which would appear in the DOM.
         // Sometimes div tags cannot be a parent or child or other tags.
         // For example, table tags. You will get a warning that the <td> cannot be a child of the <div> tag.
+        // Key attribute is the only attribute that can be applied to a fragment.
+        // There is a shorthand syntax for rendering fragments: <>, </> but the key attribute cannot be used with this shorthand.
         return (
-            <React.Fragment>
+            <React.Fragment key={"kids"}>
                 <td>Horatio</td>
                 <td>Harper</td>
             </React.Fragment>
