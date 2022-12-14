4b815910a7a86880192413339292b4385536bf0a
LeonAdeoye <leon.adeoye@gmail.com>
2022-12-14 21:36:46 +0800

    forms control submission

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -27,29 +27,38 @@ class FormComponent extends Component
         this.setState({topic: event.target.value})
     }
 
+    handleSubmit = (event) =>
+    {
+        alert(`username: ${this.state.userName}, comment: ${this.state.comment}, topic: ${this.state.topic}`);
+        event.preventDefault();
+    }
+
     render()
     {
+        // destruct values from the state
+        const {userName, comment, topic} = this.state
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div>
                     <label>Username:</label>
-                    <input type='text' value={this.state.userName} onChange={this.handleUsernameChange}/>
-                    <h2>{this.state.userName}</h2>
+                    <input type='text' value={userName} onChange={this.handleUsernameChange}/>
+                    <h2>{userName}</h2>
                 </div>
                 <div>
                     <label>Comment:</label>
-                    <textarea value={this.state.comment} onChange={this.handleCommentChange}/>
-                    <h2>{this.state.comment}</h2>
+                    <textarea value={comment} onChange={this.handleCommentChange}/>
+                    <h2>{comment}</h2>
                 </div>
                 <div>
                     <label>Topic:</label>
-                    <select value={this.state.topic} onChange={this.handleTopicChange}>
+                    <select value={topic} onChange={this.handleTopicChange}>
                         <option value="react">React</option>
                         <option value="angular">Angular</option>
                         <option value="blazor">Blazor</option>
                     </select>
-                    <h2>{this.state.topic}</h2>
+                    <h2>{topic}</h2>
                 </div>
+                <button type="submit">Submit</button>
             </form>
         );
     }
