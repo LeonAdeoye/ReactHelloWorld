85ef6842fc2ad09e293e4644c070c9601626bc45
LeonAdeoye <leon.adeoye@gmail.com>
2023-01-20 22:10:50 +0800

    HookCounterWithObject test

--- src/tests/HookCounterWithObject.test.js
+++ src/tests/HookCounterWithObject.test.js
@@ -1,5 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import HookCounterWithObject from "../components/HookCounterWithObject";
+import user from "@testing-library/user-event";
 
 describe("HookCounterWithObject", () =>
 {
@@ -20,6 +21,26 @@ describe("HookCounterWithObject", () =>
         expect(firstHeading).toBeInTheDocument();
 
         const secondHeading = screen.getByText('Object\'s JSON: {"firstName":"","lastName":""}');
+        expect(secondHeading).toBeInTheDocument();
+    })
+
+    test("updates header from text boxes", async () =>
+    {
+        user.setup();
+        render(<HookCounterWithObject/>);
+        const firstNameTextBox = screen.getByRole("textbox", {
+            name: "firstName"
+        });
+        const lastNameTextBox = screen.getByRole("textbox", {
+            name: "lastName"
+        });
+
+        await user.type(firstNameTextBox, "Horatio");
+        await user.type(lastNameTextBox, "Adey");
+
+        const firstHeading = screen.getByText("First Name: Horatio, and Last Name: Adey");
+        expect(firstHeading).toBeInTheDocument();
+        const secondHeading = screen.getByText('Object\'s JSON: {"firstName":"Horatio","lastName":"Adey"}');
         expect(secondHeading).toBeInTheDocument();
     })
 })
