84a47636fabea404efff16431f0d29a1b8b76540
LeonAdeoye <leon.adeoye@gmail.com>
2023-01-25 13:22:51 +0800

    PostList test

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,16 +1,25 @@
 import {rest} from 'msw'
 
-export const handlers = [ rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) =>
-{
-    return res(
-        ctx.status(200),
-        ctx.json([{
-            id: 1,
-            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
-        },
-        {
-            id: 2,
-            title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-        }]))
-})];
+export const handlers = [
+    rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) =>
+    {
+        return res(
+            ctx.status(200),
+            ctx.json([{
+                id: 1,
+                title: "Horatio is 10 years old."
+            },
+            {
+                id: 2,
+                title: "Harper is 6 years old.",
+            }])
+        )
+    }),
+    rest.post('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) =>
+    {
+        return res(
+            ctx.status(200)
+        )
+    })
+];
 
--- src/setupTests.js
+++ src/setupTests.js
@@ -5,7 +5,7 @@
 import '@testing-library/jest-dom';
 
 // src/setupTests.js
-import { server } from './mocks/server.js'
+import { server } from './mocks/server'
 // Establish API mocking before all tests.
 beforeAll(() => server.listen())
 
@@ -15,3 +15,13 @@ afterEach(() => server.resetHandlers())
 
 // Clean up after the tests are finished.
 afterAll(() => server.close())
+
+// You need to add the following to the package.json file:
+//   "jest": {
+//     "moduleNameMapper": {
+//       "axios": "axios/dist/node/axios.cjs"
+//     }
+//   },
+// Otherwise you will get an error:
+// syntaxError: Cannot use import statement outside a module 1 | import React, {Component} from 'react'; > 2 | import axios from "axios";
+// SEE https://stackoverflow.com/questions/73958968/cannot-use-import-statement-outside-a-module-with-axios
