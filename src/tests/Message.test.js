1331d97347fe8cd04bb216ea9c1b94aba5c6b55f
LeonAdeoye <leon.adeoye@gmail.com>
2023-01-28 23:29:01 +0800

    Added more tests for Message

--- src/tests/Message.test.js
+++ src/tests/Message.test.js
@@ -1,5 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import Message from "../components/Message";
+import user from "@testing-library/user-event";
 
 describe("Message", () =>
 {
@@ -13,5 +14,22 @@ describe("Message", () =>
 
         const subscribingHeader = screen.queryByText("Welcome Visitor!");
         expect(subscribingHeader).toBeInTheDocument();
+    })
+
+    test("reacts to button click with a thank you", async() =>
+    {
+        user.setup();
+        render(<Message/>);
+
+        const buttonElement = screen.getByRole("button", {
+            name: /subscribe/i
+        })
+        await user.click(buttonElement);
+
+        const thanksHeader = screen.queryByText("Thank you for subscribing!");
+        expect(thanksHeader).toBeInTheDocument();
+
+        const subscribingHeader = screen.queryByText("Welcome Visitor!");
+        expect(subscribingHeader).not.toBeInTheDocument();
     })
 })
