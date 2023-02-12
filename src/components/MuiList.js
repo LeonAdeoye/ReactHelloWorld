b5257dbc791c677559f5bc30fdbc473f0e33972f
LeonAdeoye <leon.adeoye@gmail.com>
2023-02-12 14:36:46 +0800

    List MUI component with tooltips.

--- src/components/MuiList.js
+++ src/components/MuiList.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Box, List, ListItem, ListItemText, ListItemIcon, ListItemAvatar, Avatar, ListItemButton, Divider} from '@mui/material'
+import {Box, List, ListItem, ListItemText, ListItemIcon, ListItemAvatar, Avatar, ListItemButton, Divider, Tooltip} from '@mui/material'
 import MailIcon from '@mui/icons-material/Mail'
 
 function MuiList()
@@ -8,45 +8,50 @@ function MuiList()
         <Box sx={{width: '400px', bgcolor: '#efefef'}}>
             <List>
                 <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <MailIcon/>
-                                </Avatar>
-                            </ListItemAvatar>
-                        </ListItemIcon>
-                        <ListItemText primary='Horatio' secondary='Ethan Horatio Adeoye'/>
-                    </ListItemButton>
+                    <Tooltip title='Horatio is a good boy!' placement='right-end' arrow enterDelay={500} leaveDelay={200}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <MailIcon/>
+                                    </Avatar>
+                                </ListItemAvatar>
+                            </ListItemIcon>
+                            <ListItemText primary='Horatio' secondary='Ethan Horatio Adeoye'/>
+                        </ListItemButton>
+                    </Tooltip>
                 </ListItem>
                 <Divider/>
                 <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <MailIcon/>
-                                </Avatar>
-                            </ListItemAvatar>
-                        </ListItemIcon>
-                        <ListItemText primary='Harper' secondary='Chloe Harper Shiori Adeoye'/>
-                    </ListItemButton>
+                    <Tooltip title='Harper is a lovely princess!' placement='right-end' arrow enterDelay={500} leaveDelay={200}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <MailIcon/>
+                                    </Avatar>
+                                </ListItemAvatar>
+                            </ListItemIcon>
+                            <ListItemText primary='Harper' secondary='Chloe Harper Shiori Adeoye'/>
+                        </ListItemButton>
+                    </Tooltip>
                 </ListItem>
                 <Divider/>
                 <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <MailIcon/>
-                                </Avatar>
-                            </ListItemAvatar>
-                        </ListItemIcon>
-                        <ListItemText primary='Saori' secondary='Saori Adeoye'/>
-                    </ListItemButton>
+                    <Tooltip title='Saori is a beautiful wife!' placement="right" arrow enterDelay={500} leaveDelay={200}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <MailIcon/>
+                                    </Avatar>
+                                </ListItemAvatar>
+                            </ListItemIcon>
+                            <ListItemText primary='Saori' secondary='Saori Adeoye'/>
+                        </ListItemButton>
+                    </Tooltip>
                 </ListItem>
             </List>
-
         </Box>
     )
 }
